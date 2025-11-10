482267bfc0c7f1dc3ce8b11d8cdc1b23e8de77de
JanielFranz <102563480+JanielFranz@users.noreply.github.com>
2025-11-10 11:09:18 -0500

    feat(iam): integrate AuthProvider with Cognito configuration

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,22 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './styles/globals.css'
+import {AuthProvider} from "react-oidc-context";
+
+const cognitoAuthConfig = {
+    authority: "https://cognito-idp.us-east-2.amazonaws.com/us-east-2_U2UOXVhrX",
+    client_id: "4ih0t549ecd43jp2nmnhakoems",
+    redirect_uri: "https://d84l1y8p4kdic.cloudfront.net",
+    response_type: "code",
+    scope: "email openid phone",
+};
+
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+      <AuthProvider {...cognitoAuthConfig}>
+          <App />
+      </AuthProvider>
   </React.StrictMode>,
 )
