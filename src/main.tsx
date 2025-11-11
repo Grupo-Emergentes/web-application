56a3c614c2f68b3250d0db43b21ea92c271e597e
JanielFranz <102563480+JanielFranz@users.noreply.github.com>
2025-11-10 21:17:53 -0500

    feat(iam): update redirect URI for local development

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import {AuthProvider} from "react-oidc-context";
 const cognitoAuthConfig = {
     authority: "https://cognito-idp.us-east-2.amazonaws.com/us-east-2_U2UOXVhrX",
     client_id: "4ih0t549ecd43jp2nmnhakoems",
-    redirect_uri: "https://d84l1y8p4kdic.cloudfront.net",
+    redirect_uri: "http://localhost:5173/",
     response_type: "code",
     scope: "email openid phone",
 };
